901da904da9ef4ef795933a62a703089e3f3896c
cyd losekann <cydnel@gmail.com>
2024-03-27 05:03:37 -0300

    controller (w/o user authentication)

--- app/controllers/seat_reservations_controller.rb
+++ app/controllers/seat_reservations_controller.rb
@@ -1,2 +1,25 @@
 class SeatReservationsController < ApplicationController
+  def new
+    @seat_reservation = SeatReservation.new
+  end
+
+  def create
+    @seat_reservation = SeatReservation.new(seat_reservation_params)
+    if @seat_reservation.save
+      redirect_to seat_reservations_path, notice: "Reservation created successfully."
+    else
+      render :new
+    end
+  end
+
+  def index
+    @seat_reservations = SeatReservation.all
+  end
+
+  private
+
+  def seat_reservation_params
+    params.require(:seat_reservation).permit(:seat_number, :show_id)
+    # autenticação do usuário a ser incluída aqui
+  end
 end
--- app/models/seat_reservation.rb
+++ app/models/seat_reservation.rb
@@ -1,4 +1,3 @@
 class SeatReservation < ApplicationRecord
   belongs_to :show
-  belongs_to :user
 end
