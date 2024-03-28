d2b937217225f0509fc69aeb6c42c5298e8dd338
cyd losekann <cydnel@gmail.com>
2024-03-28 05:09:20 -0300

    validations

--- app/models/seat_reservation.rb
+++ app/models/seat_reservation.rb
@@ -1,4 +1,11 @@
 class SeatReservation < ApplicationRecord
+
   belongs_to :show
-  validates :seat_position, uniqueness: { scope: :show_id }
+  belongs_to :user, optional: true
+
+
+  validates :seat_position, presence: true
+  validates :seat_position, uniqueness: { scope: :show_id, message: "seat is already taken for this show" }
+  validate :seat_must_be_available, on: :create
+
 end
