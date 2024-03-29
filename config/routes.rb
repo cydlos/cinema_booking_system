a7e6488fd65fd25737ae4675a2abfc1cd03ba6c9
cyd losekann <cydnel@gmail.com>
2024-03-29 05:49:44 -0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  root 'cinemas#index'
+
+  resources :cinemas do
+    resources :shows, only: [:index, :show]
+  end
+
+  resources :shows do
+    resources :seat_reservations, only: [:new, :create, :index]
+  end
 
 end
