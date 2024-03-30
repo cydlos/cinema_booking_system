e049ac6022c9c7fa89a4323968daf18205e94337
cyd losekann <cydnel@gmail.com>
2024-03-30 03:07:01 -0300

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 user1 = User.create!(email: 'user1@example.com', password: 'password', password_confirmation: 'password')
 user2 = User.create!(email: 'user2@example.com', password: 'password2', password_confirmation: 'password2')
 
-cinema1 = Cinema.create!(name: 'Cinema Paradiso', location: 'Downtown')
-cinema2 = Cinema.create!(name: 'The Appartment', location: 'Uptown')
+cinema1 = Cinema.create!(name: 'Kinoplex', location: 'Downtown')
+cinema2 = Cinema.create!(name: 'Severiano Ribeiro', location: 'Uptown')
 
-show1 = Show.create!(movie_title: 'Kinoplex', start_time: DateTime.now + 5.days, cinema: cinema1)
-show2 = Show.create!(movie_title: 'Kinoplex 2', start_time: DateTime.now + 3.days, cinema: cinema2)
+show1 = Show.create!(movie_title: 'The Appartment', start_time: DateTime.now + 5.days, cinema: cinema1)
+show2 = Show.create!(movie_title: 'Saw 1', start_time: DateTime.now + 3.days, cinema: cinema2)
