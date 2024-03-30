7bd2a7dd089221da54abba71060e6bba95c78d63
cyd losekann <cydnel@gmail.com>
2024-03-30 03:17:46 -0300

    new migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 user1 = User.create!(email: 'user1@example.com', password: 'password', password_confirmation: 'password')
 user2 = User.create!(email: 'user2@example.com', password: 'password2', password_confirmation: 'password2')
 
-cinema1 = Cinema.create!(name: 'Kinoplex', location: 'Downtown')
+cinema1 = Cinema.create!(name: 'Kinoplex', location: 'Leblon')
 cinema2 = Cinema.create!(name: 'Severiano Ribeiro', location: 'Uptown')
 
 show1 = Show.create!(movie_title: 'The Appartment', start_time: DateTime.now + 5.days, cinema: cinema1)
