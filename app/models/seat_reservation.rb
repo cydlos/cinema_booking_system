a154d8f8b0e6a02d95820d5f065ffacaf32c4f3c
cyd losekann <cydnel@gmail.com>
2024-04-02 03:58:33 -0300

    "syntax corrected"

--- app/models/seat_reservation.rb
+++ app/models/seat_reservation.rb
@@ -1,8 +1,6 @@
 class SeatReservation < ApplicationRecord
-
   belongs_to :show
   belongs_to :user, optional: true
-
 
   validates :seat_position, presence: true
   validates :seat_position, uniqueness: { scope: :show_id, message: "seat is already taken for this show" }
